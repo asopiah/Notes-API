07dd2634e92a4ff17bfce61ee6133e6531700bba
Alfred Sopia <sopiaah100@gmail.com>
2019-05-22 14:05:12 +0300

    Enabled JPA Auditing in the main application

--- src/main/java/NotesAPIApplication.java
+++ src/main/java/NotesAPIApplication.java
@@ -1,8 +1,11 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
+@EnableJpaAuditing //Enable JPA Auditing in the main application.
+
 public class NotesAPIApplication {
 
     public static void main(String[] args) {
