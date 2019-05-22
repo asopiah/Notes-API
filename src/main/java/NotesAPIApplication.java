1770a815f01ebda6a5f5d96576f421313af6984c
Alfred Sopia <sopiaah100@gmail.com>
2019-05-22 13:56:42 +0300

    Coorected main class

--- src/main/java/NotesAPIApplication.java
+++ src/main/java/NotesAPIApplication.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@SpringBootApplication
 public class NotesAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesAPIApplication.class, args);
+    }
 }
