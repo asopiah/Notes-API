75ee81fda4701a33d8b0d6a0792856a4ea883dcb
Alfred Sopia <sopiaah100@gmail.com>
2019-05-22 14:32:04 +0300

    Added appropriate Rest API annoations

--- src/main/java/controller/NoteController.java
+++ src/main/java/controller/NoteController.java
@@ -1,4 +1,20 @@
 package controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // this is a combination of Spring’s @Controller and @ResponseBody annotations
+@RequestMapping("/api") //declares that the url for all the apis in this controller will start with /api
 public class NoteController {
+
+    // Get All Notes 
+
+    // Create a new Note
+
+    // Get a Single Note
+
+    // Update a Note
+
+    // Delete a Note
 }
