a6f5f220e2c13817ac13485031f3fe54a78c6fce
Alfred Sopia <sopiaah100@gmail.com>
2019-05-23 00:49:22 +0300

    Final commit

--- src/main/java/com/sopia/notesapi/NotesAPIApplication.java
+++ src/main/java/com/sopia/notesapi/NotesAPIApplication.java
@@ -2,10 +2,11 @@ package com.sopia.notesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
-//@EnableJpaAuditing //Enable JPA Auditing in the main application.
+@EnableJpaAuditing //Enable JPA Auditing in the main application.
 
 public class NotesAPIApplication {
 
